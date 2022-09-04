aa94791febe76704b5b24b1a27c664e8ee3bfd63
ivan <ivansyn2013@yandex.ru>
2022-09-05 00:51:07 +0300

    add db table sample and change url

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import view
 
 urlpatterns = [
+    path('', view.index, name='index'),
     path('admin/', admin.site.urls),
 ]
